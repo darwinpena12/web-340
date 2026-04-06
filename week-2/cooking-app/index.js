b34950fb2adaa31463b46d2ea622d35e4cd47434
Darwin <darwinpena12@gmail.com>
2026-04-05 21:58:31 -0400

    initial commit

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,16 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Darwin Pena Cabrera
+ * Date: 04/05/2026
+ * File Name: index.js
+ * Description: file to demonstrate the functionality of the modules
 */
 
 // TODO: Import your module using require
 
+const recipes = require("./recipes");
+
 // TODO: Implement your CLI program here
+const ingredients = ["ingredient1", "ingredient2"]
+console.log(recipes.createRecipe(ingredients));
+console.log(recipes.setTimer(15));
+console.log(recipes.quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,32 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Darwin Pena Cabrera
+ * Date: 04/05/2026
+ * File Name: recipes.js
+ * Description: Function to run the recipe application
 */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
+  return "Recipe created with ingredients: " + ingredients.join(", ");
   // TODO: Implement this function
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
+  return "Timer set for " + minutes + " minutes";
   // TODO: Implement this function
 }
 
 // Define the quit function
 function quit() {
   // TODO: Implement this function
+  return "Program exited";
 }
 
 // TODO: Export the functions
+
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit
+};
