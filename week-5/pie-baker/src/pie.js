c7808733960318e74ec8e14b11773345a0af981c
Darwin <darwinpena12@gmail.com>
2026-04-27 04:26:03 -0400

    initial commit

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,23 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Darwin Pena Cabrera  
+ * Date: 04/26/2026
+ * File Name: pie.js
+ * Description: Index file
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
+  const essentialIngredients = ["flour", "sugar", "butter"];
+
+  // Verifying that all the essential ingredients are included
+  const missing = essentialIngredients.filter(item => !ingredients.includes(item));
+
+  if (missing.length > 0 ) {
+    console.warn(`Missing essential ingredients: ${missing.join(", ")}`);
+    process.exit(1);
+  }
+
+  return `Success: ${pieType} pie baked successfully with ${ingredients.join(", ")}.`;
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,54 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Darwin Pena Cabrera
+ * Date: 04/26/2026
+ * File Name: pie.spec.js
+ * Description: test file
  */
 
 "use strict";
 
 const { bakePie } = require("../src/pie");
 
-// Your tests here
+// test for process.exit
+
+const exitMock = jest.spyOn(process, "exit").mockImplementation((code) => code);
+
+let log;
+
+beforeEach(() => {
+  log = jest.spyOn(console, "warn");
+});
+
+afterEach(() => {
+  log.mockRestore();
+});
+
+afterAll(() => {
+  exitMock.mockRestore();
+});
+
+describe("bakePie function", () => {
+
+  test("successfully bakes pie when all essential ingredients are present", () => {
+    const result = bakePie("apple", ["flour", "sugar", "butter", "apple"]);
+
+    expect(result).toBe("Success: apple pie baked successfully with flour, sugar, butter, apple.");
+    
+    expect(exitMock).not.toHaveBeenCalled();
+  });
+
+  test("failure due to missing one or more essential ingredient", () => {
+    bakePie("apple", ["flour", "sugar"]);
+
+    expect(exitMock).toHaveBeenCalledWith(1);
+    expect(log).toHaveBeenCalled();
+  });
+
+  test("Returns correct success message", () => {
+    const result = bakePie("cherries", ["flour", "sugar", "butter", "cherries"]);
+
+    expect(result).toBe("Success: cherries pie baked successfully with flour, sugar, butter, cherries.");
+  });
+});
+
+
